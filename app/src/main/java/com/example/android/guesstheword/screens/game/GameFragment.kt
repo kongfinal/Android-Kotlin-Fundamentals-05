7fc51d170d319bb85ff3a1f9adff59396b930f02
59160927 <59160927@go.buu.ac.th>
2019-09-26 07:55:30 -0700

    Add ViewModel data binding

--- app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
@@ -56,9 +56,8 @@ class GameFragment : Fragment() {
         viewModel.resetList()
         viewModel.nextWord()
 
-        binding.correctButton.setOnClickListener { onCorrect() }
-        binding.skipButton.setOnClickListener { onSkip() }
-        binding.endGameButton.setOnClickListener { onEndGame() }
+        binding.gameViewModel = viewModel
+
 
         viewModel.score.observe(this, Observer { newScore ->
             binding.scoreText.text = newScore.toString()
@@ -74,18 +73,6 @@ class GameFragment : Fragment() {
 
         return binding.root
 
-    }
-
-    private fun onSkip() {
-        viewModel.onSkip()
-    }
-    private fun onCorrect() {
-        viewModel.onCorrect()
-    }
-
-
-    private fun onEndGame() {
-        gameFinished()
     }
 
     private fun gameFinished() {
