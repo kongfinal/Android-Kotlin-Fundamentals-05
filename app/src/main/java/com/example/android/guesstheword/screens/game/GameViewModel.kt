183841affb22d6127251b50ac9d7c539c0c8387f
59160927 <59160927@go.buu.ac.th>
2019-09-26 06:59:40 -0700

    Encapsulate LiveData

--- app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -1,24 +1,29 @@
 package com.example.android.guesstheword.screens.game
 
 import android.util.Log
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class GameViewModel : ViewModel() {
 
     // The current word
-    var word = MutableLiveData<String>()
+    private val _word = MutableLiveData<String>()
+    val word:LiveData<String>
+        get()  = _word
 
     // The current score
-    var score = MutableLiveData<Int>()
+    private val _score = MutableLiveData<Int>()
+    val score:LiveData<Int>
+        get()  = _score
 
     // The list of words - the front of the list is the next word to guess
     private lateinit var wordList: MutableList<String>
 
     init {
         Log.i("GameViewModel", "GameViewModel created!")
-        word.value = ""
-        score.value = 0
+        _word.value = ""
+        _score.value = 0
     }
 
     override fun onCleared() {
@@ -60,14 +65,14 @@ class GameViewModel : ViewModel() {
 
     fun onSkip() {
         if (!wordList.isEmpty()) {
-            score.value = (score.value)?.minus(1)
+            _score.value = (_score.value)?.minus(1)
         }
         nextWord()
     }
 
     fun onCorrect() {
         if (!wordList.isEmpty()) {
-            score.value = (score.value)?.plus(1)
+            _score.value = (_score.value)?.plus(1)
         }
         nextWord()
     }
@@ -78,7 +83,7 @@ class GameViewModel : ViewModel() {
     fun nextWord() {
         if (!wordList.isEmpty()) {
             //Select and remove a word from the list
-            word.value = wordList.removeAt(0)
+            _word.value = wordList.removeAt(0)
         }
     }
 }
